c612fe1d121237094de822b31aaf6ab205bbea64
Talison Moura <talisonmoura13@gmail.com>
2023-09-18 23:33:40 -0300

    Estrutura base para receber um User

---
+++ UsersAPI/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsersAPI.Data.Dtos;
+
+namespace UsersAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateUser(CreateUserDto userDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UsersAPI/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace UsersAPI.Models
+{
+    public class User
+    {
+
+    }
+}
